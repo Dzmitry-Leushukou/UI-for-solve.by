5a46481856381ad23413eba8aa401eb80ee98d3c
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-05-26 14:47:07 +0300

    Implemented directory edit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,47 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::showDirectory()
+{
+    ui->directory_line->setText(directory_name);
+
+    colorise_directory_line();
+}
+
+void MainWindow::colorise_directory_line()
+{
+    if(!correct_directory())
+    {
+        ui->directory_line->setStyleSheet(incorrect_directory_style);
+        return;
+    }
+
+    ui->directory_line->setStyleSheet(correct_directory_style);
+}
+
+bool MainWindow::correct_directory()
+{
+    QDir dir(directory_name);
+
+    if(dir.exists())
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void MainWindow::on_ChangeDirectoryButton_clicked()
+{
+    directory_name = QFileDialog::getExistingDirectory(this, tr("Choose directory"),directory_name,QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    showDirectory();
+}
+
+
+void MainWindow::on_directory_line_textEdited(const QString &arg1)
+{
+    directory_name=arg1;
+    colorise_directory_line();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -14,10 +15,31 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    //Constructor and destructor
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    //Methods
+    void showDirectory();
+    void colorise_directory_line();
+
+    bool correct_directory();
+
+
+private slots:
+
+
+    void on_ChangeDirectoryButton_clicked();
+
+    void on_directory_line_textEdited(const QString &arg1);
+
 private:
+    //Fields
     Ui::MainWindow *ui;
+    QString directory_name;
+
+    //Style
+    const QString incorrect_directory_style="color: rgb(224, 27, 36);";
+    const QString correct_directory_style="color: rgb(51, 209, 122);";
 };
 #endif // MAINWINDOW_H
